1ae45b0657e32a0adc681bff09aa273d7e573987
Matheus Barretto <matheusfbarretto@gmail.com>
2025-05-05 18:50:16 -0300

    Update cadastrousuario.php

--- cadastrousuario.php
+++ cadastrousuario.php
@@ -7,28 +7,29 @@ require_once ('includes/topo.php');
     <div>
         <form name="cadatro-usuario" action="inserirusuario.php" method="post">
             <label for="nome">Nome:</label><br>
-            <input name="nome"type="text" require="true" placeholder="Nome completo" size ="56" value="">
+            <input id="nome" name="nome" type="text"  placeholder="Nome completo" size ="56" value="" required>
             <br>
             <label for="email">Email:</label><br>
-            <input name="emial"type="email" require="true" placeholder="Email@email.com" size="56" value="">
+            <input id="email" name="email" type="email"  placeholder="Email@email.com" size="56" value=""required>
             <br>
             <label for="cpf">Cpf:</label><br>
-            <input name="cpf"type="cpf" require="true" placeholder="Cpf" size="56" value="">
+            <input id="cpf"name="cpf" type="text"  placeholder="Cpf" size="56" value=""required>
             <br>
             <label for="endereco">Endereco:</label><br>
-            <input name="endereco"type="text" require="true" placeholder="Ex: Rua das flores, 1010, Jardim das Flores" size="56" value="">
+            <input id="endereco"name="endereco"type="text"  placeholder="Ex: Rua das flores, 1010, Jardim das Flores" size="56" value=""required>
             <br>
             <label for="login">Login:</label><br>
-            <input name="login" type="text" require="true" placeholder="Login"size="56" value="">
+            <input id="login"name="login" type="text"  placeholder="Login"size="56" value=""required>
             <br>    
             <label for="senha">Senha:</label><br>
-            <input name="senha"type="password" require="true" placeholder="Senha"size="56" value="">
+            <input id="senha"name="senha"type="password"  placeholder="Senha"size="56" value=""required>
             <br>
-            <label for="confirmar-senha">Confirme sua senha:</label><br>
-            <input name="confirmar-senha" type="password" require="true" placeholder="Confirmação"size="56" value="">
+            <label for="confirmar_senha">Confirme sua senha:</label><br>
+            <input id="confirmar_senha"name="confirmar_senha" type="password"  placeholder="Confirmação"size="56" value=""required>
             <br><br>
-            <input type="submit" value="Cadastrar">
+            <input type="submit" name="Enviar"value="Cadastrar">
             <input type="reset" value="Limpar">
+
         </form>  
     </div>
 <body>
